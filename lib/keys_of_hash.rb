6d084cf0df7c38693abde6ac9258564f750e2963
Jasterix C <loune.calixte@gmail.com>
2019-04-17 08:10:32 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     arr = []
-    self.map |k, v|
+    self.each |k, v|
     arguments.select |i|
       if i == v
         arr.unshift(k)
